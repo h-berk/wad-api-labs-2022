64362d1406e46afe9e4baa34e7cd9d5b86c0fe06
Hasan Berk <hasanberk50@gmail.com>
2022-11-26 07:44:34 +0000

    added routing for movies and movie reviews

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './api/movies';
+import moviesRouter from './api/movies/index.js';
 
 dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 
+app.use(express.json());
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
