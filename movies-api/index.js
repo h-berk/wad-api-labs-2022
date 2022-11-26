ee630606e46508457ec976e8ac4bd713325dbf4f
Hasan Berk <hasanberk50@gmail.com>
2022-11-26 08:04:44 +0000

    Mongo lab Users Router

--- movies-api/index.js
+++ movies-api/index.js
@@ -4,6 +4,7 @@ import moviesRouter from './api/movies/index.js';
 import genresRouter from './api/genres/index.js';
 import './db/index.js';
 import './seedData/index.js';
+import usersRouter from './api/users/index.js';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
