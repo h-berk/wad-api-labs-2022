0552371e1ea275b50964054068f6ff7f1f29bb2f
Hasan Berk <hasanberk50@gmail.com>
2022-11-26 08:10:33 +0000

    Mongo lab Error Handling

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import User from './userModel.js';
 
 const router = express.Router(); // eslint-disable-line
@@ -9,24 +10,24 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
-// register(Create)/Authenticate User
-router.post('/', async (req, res) => {
-if (req.query.action === 'register') {  //if action is 'register' then save to DB
-    await User(req.body).save();
-    res.status(201).json({
-        code: 201,
-        msg: 'Successful created new user.',
-    });
-}
-else {  //Must be an authenticate then!!! Query the DB and check if there's a match
-    const user = await User.findOne(req.body);
-    if (!user) {
-        return res.status(401).json({ code: 401, msg: 'Authentication failed' });
-    }else{
-        return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' });
+  // register
+  router.post('/', asyncHandler(async (req, res) => {
+    if (req.query.action === 'register') {  //if action is 'register' then save to DB
+        await User(req.body).save()
+        res.status(201).json({
+            code: 201,
+            msg: 'Successful created new user.',
+        });
     }
-}
-});
+    else {  //Must be authenticating the!!! Query the DB and check if there's a match
+        const user = await User.findOne(req.body);
+        if (!user) {
+            return res.status(401).json({ code: 401, msg: 'Authentication failed' })
+        } else {
+            return res.status(200).json({ code: 200, msg: "Authentication Successful", token: 'TEMPORARY_TOKEN' })
+        }
+    }
+}));
 
   // Update a user
   router.put('/:id', async (req, res) => {
--- movies-api/index.js
+++ movies-api/index.js
@@ -12,10 +12,21 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+// eslint-disable-next-line no-unused-vars
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
